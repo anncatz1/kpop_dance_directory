ea139e8ab35cb5eb22a07cd4902f20dc75191204
Annie Xu <anniexu@Annies-MacBook-Pro-4.local>
2023-10-06 16:27:43 -0500

    sorting

--- src/components/VideosTable.jsx
+++ src/components/VideosTable.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useSearchParams } from "react-router-dom";
-import { useQuery, useQueryClient, useInfiniteQuery } from "react-query";
+import { useQuery } from "react-query";
 
 import supabase from "../services/supabase";
 import VideoRow from "./VideoRow";
@@ -35,34 +35,12 @@ const TableHeader = styled.header`
 `;
 
 function VideosTable() {
-  // const [videos, setVideos] = useState([]);
   const [totalVideos, setTotalVideos] = useState(0);
-  // const [page, setPage] = useState(1);
   const [searchParams, setSearchParams] = useSearchParams();
   const page = parseInt(searchParams.get("page")) || 1;
+  const artist = searchParams.get("artist") || "All";
+  const sortBy = searchParams.get("sort") || "date-desc";
 
-  // const queryClient = useQueryClient();
-
-  // Queries
-  // const { isLoading, data: videos } = useQuery({
-  //   queryKey: ["dances"],
-  //   queryFn: fetchVideos,
-  //   // ...config,
-  // });
-
-  // const {
-  //   data: videos,
-  //   error,
-  //   fetchNextPage,
-  //   hasNextPage,
-  //   isFetching,
-  //   isFetchingNextPage,
-  //   status,
-  // } = useInfiniteQuery({
-  //   queryKey: ["dancesInf"],
-  //   queryFn: fetchVideos,
-  //   getNextPageParam: (lastPage, pages) => pages.length,
-  // });
   // select: (data) => ({
   //   pages: [...data.pages].reverse(),
   //   pageParams: [...data.pageParams].reverse(),
@@ -75,30 +53,55 @@ function VideosTable() {
     isFetching,
     isPreviousData,
   } = useQuery({
-    queryKey: ["dances", page],
-    queryFn: () => fetchVideos(page),
+    queryKey: ["dances", page, sortBy],
+    queryFn: () => fetchVideos(page, sortBy),
     keepPreviousData: true,
   });
 
-  async function fetchVideos(pageParam) {
+  const totalPages = Math.ceil(totalVideos / ITEMS_PER_PAGE);
+
+  async function fetchVideos(pageParam, sortBy = "date-desc") {
     try {
       const start = (pageParam - 1) * ITEMS_PER_PAGE;
-
+      const [field, direction] = sortBy.split("-");
+      // const modifier = direction === "asc" ? 1 : -1;
       const { data, count, error } = await supabase
         .from("dances")
         .select("*", { count: "exact" })
+        .order(field, { ascending: direction === "asc" })
         .range(start, start + ITEMS_PER_PAGE - 1);
-      // .range(pageParam, pageParam);
-      // console.log("page", pageParam - 1);
 
+      // dataHere = data;
+
+      // let dataHere;
+      // let countHere;
+      // let errorHere;
+
+      // if (artist !== "All") {
+      //   const { data, count, error } = await supabase
+      //     .from("dances")
+      //     .select("*", { count: "exact" })
+      //     .order(field, { ascending: direction === "asc" })
+      //     .range(start, start + ITEMS_PER_PAGE - 1)
+      //     .eq("artist", artist);
+
+      //   dataHere = data;
+      // } else {
+      //   const { data, count, error } = await supabase
+      //     .from("dances")
+      //     .select("*", { count: "exact" })
+      //     .order(field, { ascending: direction === "asc" })
+      //     .range(start, start + ITEMS_PER_PAGE - 1);
+
+      //   dataHere = data;
+      // }
+
+      // .range(pageParam, pageParam);
       // .eq("mirror", true);
       // .order("date", { ascending: false })
 
       if (error) throw error;
-      // setVideos(data);
-
       setTotalVideos(count);
-      // console.log("supabase", data);
       return data;
     } catch (error) {
       console.error("Failed to fetch videos:", error);
@@ -107,26 +110,18 @@ function VideosTable() {
 
   const handlePrevious = () => {
     if (page > 1) {
-      // setPage(page - 1);
       searchParams.set("page", page - 1);
       setSearchParams(searchParams);
     }
-    // () => setPage((old) => Math.max(old - 1, 1))
   };
 
   const handleNext = () => {
     if (!isPreviousData && page < totalPages) {
-      // setPage(page + 1);
       searchParams.set("page", page + 1);
       setSearchParams(searchParams);
     }
-    // if () {
-    //   setPage((old) => old + 1);
-    // }
   };
 
-  // const { isLoading, cabins } = useCabins();
-  // if (isLoading) return <Spinner />;
   // filter
 
   // const mirror = searchParams.get("mirrored");
@@ -150,8 +145,6 @@ function VideosTable() {
   // function compareNumbers(a, b) {
   //   return (a[field] - b[field]) * modifier;
   // }
-  // console.log(videos);
-  if (isLoading) return <Spinner />;
 
   // const sortBy = searchParams.get("sort") || "date-desc";
   // const [field, direction] = sortBy.split("-");
@@ -160,8 +153,8 @@ function VideosTable() {
 
   // let filteredVideos;
   // const filterArtist = searchParams.get("artist") || "All";
-  // if (filterArtist === "All") filteredVideos = sortedVideos;
-  // else filteredVideos = videos.filter((video) => video.artist === filterArtist);
+  // if (artist === "All") filteredVideos = videos;
+  // else filteredVideos = videos.filter((video) => video.artist === artist);
 
   // const start = (currentPage - 1) * ITEMS_PER_PAGE;
   // const rangeVids = filteredVideos.slice(start, start + ITEMS_PER_PAGE);
@@ -170,12 +163,12 @@ function VideosTable() {
   //   ? filteredCabins.sort(compareNumbers)
   //   : filteredCabins.sort(compareText);
 
-  const totalPages = Math.ceil(totalVideos / ITEMS_PER_PAGE);
-
   function handlePage(event, value) {
     searchParams.set("page", value);
     setSearchParams(searchParams);
   }
+
+  if (isLoading) return <Spinner />;
 
   return (
     <>
@@ -185,34 +178,13 @@ function VideosTable() {
           <div>Date</div>
           <div>Practice</div>
           <div>Tutorials </div>
-          {/* <div>Tutorial Pt. 2</div> */}
         </TableHeader>
 
         {videos.map((row, index) => (
           <VideoRow key={index} row={row} />
         ))}
-
-        {/* {videos.pages.map((group, i) => (
-          <React.Fragment key={i}>
-            {group.map((row, index) => (
-              <VideoRow key={index} row={row} />
-            ))}
-          </React.Fragment>
-        ))} */}
       </Table>
-      {/* <button
-        className="bg-purple-400 text-white px-4 py-2 rounded hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-purple-300 focus:ring-opacity-50"
-        onClick={() => fetchNextPage()}
-        disabled={!hasNextPage || isFetchingNextPage}
-      >
-        {isFetchingNextPage
-          ? "Loading more..."
-          : hasNextPage
-          ? "Load More"
-          : "Nothing more to load"}
-      </button>
-      <div>{isFetching && !isFetchingNextPage ? "Fetching..." : null}</div> */}
-      {/* <span>Current Page: {page}</span> */}
+
       <div className="flex justify-between my-3">
         <button
           className="bg-purple-400 text-white px-4 py-2 rounded hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-purple-300 focus:ring-opacity-50"
@@ -229,6 +201,7 @@ function VideosTable() {
           Next Page
         </button>
       </div>
+
       <Typography>Page: {page}</Typography>
       <Pagination
         count={totalPages}
@@ -238,28 +211,6 @@ function VideosTable() {
         showFirstButton
         showLastButton
       />
-
-      {/* {isFetching ? <span> Loading...</span> : null}{" "} */}
-      {/* <div className="flex justify-between mt-4">
-        <button
-          onClick={handlePrevious}
-          disabled={currentPage === 1}
-          className="bg-purple-400 text-white px-4 py-2 rounded mr-2 hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-purple-300 focus:ring-opacity-50 disabled:opacity-50"
-        >
-          Previous
-        </button>
-        <button
-          onClick={handleNext}
-          className="bg-purple-400 text-white px-4 py-2 rounded hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-purple-300 focus:ring-opacity-50"
-        >
-          Next
-        </button>
-      </div> */}
-      {/* <Pagination
-        currentPage={currentPage}
-        totalVideos={totalVideos}
-        onPageChange={setCurrentPage}
-      />  */}
     </>
   );
 }
