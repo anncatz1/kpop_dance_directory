680fadc41e2e7018b87c64532f350a442bd3ddea
Annie Xu <anniexu@Annies-MacBook-Pro-4.local>
2023-10-09 13:41:58 -0500

    Fixes

--- src/components/VideosTable.jsx
+++ src/components/VideosTable.jsx
@@ -41,11 +41,8 @@ function VideosTable() {
   const page = parseInt(searchParams.get("page")) || 1;
   const filterArtist = searchParams.get("artist") || "All";
   const sortBy = searchParams.get("sort") || "date-desc";
+  const totalPages = Math.ceil(totalVideos / ITEMS_PER_PAGE);
 
-  // select: (data) => ({
-  //   pages: [...data.pages].reverse(),
-  //   pageParams: [...data.pageParams].reverse(),
-  // }),
   const {
     isLoading,
     isError,
@@ -57,23 +54,12 @@ function VideosTable() {
     queryKey: ["dances", page, sortBy, filterArtist],
     queryFn: () => fetchVideos(page, sortBy),
     keepPreviousData: true,
-    // select: (data) => data.filter((item) => item.artist === filterArtist),
   });
-
-  const totalPages = Math.ceil(totalVideos / ITEMS_PER_PAGE);
 
   async function fetchVideos(pageParam, sortBy = "date-desc") {
     try {
       const start = (pageParam - 1) * ITEMS_PER_PAGE;
       let [field, direction] = sortBy.split("-");
-      // const modifier = direction === "asc" ? 1 : -1;
-
-      // const { data, error: error2 } = await supabase.rpc(
-      //   create function lower_name(s_users_lists s) returns text as $$
-      //   select lower(s.name);
-      // $$ language sql;
-      // );
-      // console.log(data, error2);
       if (field === "artist") field = "lower_artist";
 
       const {
@@ -84,62 +70,38 @@ function VideosTable() {
         .from("dances")
         .select("*", { count: "exact" })
         .order(field, { ascending: direction === "asc" });
-      // .range(start, start + ITEMS_PER_PAGE - 1);
-      // console.log(videos);
 
       let filteredVideos;
       if (filterArtist === "All") filteredVideos = videos;
-      else
+      else {
         filteredVideos = videos.filter(
           (video) => video.artist === filterArtist
         );
+        setPageTo1();
+      }
 
-      const rangeVids = filteredVideos.slice(start, start + ITEMS_PER_PAGE);
-
-      // if (artist !== "All") {
-      //   const { data, count, error } = await supabase
-      //     .from("dances")
-      //     .select("*", { count: "exact" })
-      //     .order(field, { ascending: direction === "asc" })
-      //     .range(start, start + ITEMS_PER_PAGE - 1)
-      //     .eq("artist", artist);
-
-      //   dataHere = data;
-      // } else {
-      //   const { data, count, error } = await supabase
-      //     .from("dances")
-      //     .select("*", { count: "exact" })
-      //     .order(field, { ascending: direction === "asc" })
-      //     .range(start, start + ITEMS_PER_PAGE - 1);
-
-      //   dataHere = data;
-      // }
-
-      // .range(pageParam, pageParam);
-      // .eq("mirror", true);
-      // .order("date", { ascending: false })
-      // if (filterArtist !== "All") setPageTo1();
       if (error) throw error;
-      setTotalVideos(count);
-      return rangeVids;
+      setTotalVideos(filteredVideos.length);
+      return filteredVideos;
     } catch (error) {
       console.error("Failed to fetch videos:", error);
     }
   }
+  const start = (page - 1) * ITEMS_PER_PAGE;
+  const rangeVids = videos.slice(start, start + ITEMS_PER_PAGE);
+  // const handlePrevious = () => {
+  //   if (page > 1) {
+  //     searchParams.set("page", page - 1);
+  //     setSearchParams(searchParams);
+  //   }
+  // };
 
-  const handlePrevious = () => {
-    if (page > 1) {
-      searchParams.set("page", page - 1);
-      setSearchParams(searchParams);
-    }
-  };
-
-  const handleNext = () => {
-    if (!isPreviousData && page < totalPages) {
-      searchParams.set("page", page + 1);
-      setSearchParams(searchParams);
-    }
-  };
+  // const handleNext = () => {
+  //   if (!isPreviousData && page < totalPages) {
+  //     searchParams.set("page", page + 1);
+  //     setSearchParams(searchParams);
+  //   }
+  // };
 
   // filter
 
@@ -150,37 +112,6 @@ function VideosTable() {
   //   dancePractices = videos.filter((cabin) => cabin.discount === 0);
   // if (filterValue === "with-discount")
   //   filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
-
-  // sort by
-  // function compareText(a, b) {
-  //   if (a[field].toLowerCase() < b[field].toLowerCase()) {
-  //     return -1 * modifier;
-  //   }
-  //   if (a[field].toLowerCase() > b[field].toLowerCase()) {
-  //     return 1 * modifier;
-  //   }
-  //   return 0;
-  // }
-  // function compareNumbers(a, b) {
-  //   return (a[field] - b[field]) * modifier;
-  // }
-
-  // const sortBy = searchParams.get("sort") || "date-desc";
-  // const [field, direction] = sortBy.split("-");
-  // const modifier = direction === "asc" ? 1 : -1;
-  // const sortedVideos = videos.sort(compareText);
-
-  // let filteredVideos;
-  // const filterArtist = searchParams.get("artist") || "All";
-  // if (artist === "All") filteredVideos = videos;
-  // else filteredVideos = videos.filter((video) => video.artist === artist);
-
-  // const start = (currentPage - 1) * ITEMS_PER_PAGE;
-  // const rangeVids = filteredVideos.slice(start, start + ITEMS_PER_PAGE);
-
-  // typeof videos[0][field] === "number"
-  //   ? filteredCabins.sort(compareNumbers)
-  //   : filteredCabins.sort(compareText);
 
   function setPageTo1() {
     searchParams.set("page", 1);
@@ -204,7 +135,7 @@ function VideosTable() {
           <div>Tutorials </div>
         </TableHeader>
 
-        {videos.map((row, index) => (
+        {rangeVids.map((row, index) => (
           <VideoRow key={index} row={row} />
         ))}
       </Table>
@@ -226,7 +157,6 @@ function VideosTable() {
         </button>
       </div> */}
 
-      {/* <Typography>Page: {page}</Typography> */}
       <Pagination
         count={totalPages}
         page={page}
